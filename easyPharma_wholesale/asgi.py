f278f304e1137f5f2057cfe3150589f923e97123
=farooq <=farooqaziz1993@gmail.com>
2026-06-28 13:21:07 +0530

    first commit

---
+++ easyPharma_wholesale/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for easyPharma_wholesale project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'easyPharma_wholesale.settings')
+
+application = get_asgi_application()
